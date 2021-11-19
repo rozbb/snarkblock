00eb239a7c7290a37921c99618b43f253b06a8bc
Michael Rosenberg <michael@mrosenberg.pub>
2021-11-19 01:20:14 -0500

    Chunk proof and verif throughput benches compiling

--- benches/bench.rs
+++ benches/bench.rs
@@ -12,12 +12,77 @@ use rayon::prelude::*;
 const NUM_CHUNKS_TO_TEST: &[usize] = &[16 - 2, 64 - 2, 256 - 2, 1024 - 2, 4096 - 2, 16384 - 2];
 const CHUNK_SIZES_TO_TEST: &[usize] = &[1024, 8192];
 
+// This measures the cost of doing up to 14 16-element chunk proofs in parallel, as well as the
+// cost of doing just 1 chunk proof of larger sizes
+fn chunk_proof(c: &mut Criterion) {
+    let mut rng = test_rng();
+
+    // Generate a fresh private ID
+    let priv_id = PrivateId::gen(&mut rng);
+
+    // Do the parallel experiments with the 16-elem chunks
+    {
+        let chunk_size = 16;
+        let chunk = Chunk::gen_with_size(&mut rng, chunk_size);
+
+        let (chunk_pk, _) = chunk_setup(&mut rng, chunk_size);
+        let chunk_prover = ChunkProver {
+            priv_id,
+            proving_key: chunk_pk,
+        };
+
+        for num_chunks in 1..=14 {
+            c.bench_function(
+                &format!("Proving {} chunk(s) [cs={}]", num_chunks, chunk_size),
+                |b| {
+                    b.iter(|| {
+                        // Do num_chunks chunk proofs in parallel
+                        (0..num_chunks).into_par_iter().for_each(|_| {
+                            let mut rng = test_rng();
+                            chunk_prover
+                                .prove(&mut rng, &chunk)
+                                .expect("couldn't prove chunk");
+                        })
+                    })
+                },
+            );
+        }
+    }
+
+    // Do the experiments with just 1 chunk of varying size
+    for &chunk_size in CHUNK_SIZES_TO_TEST {
+        let num_chunks = 1;
+        let chunk = Chunk::gen_with_size(&mut rng, chunk_size);
+
+        let (chunk_pk, _) = chunk_setup(&mut rng, chunk_size);
+        let chunk_prover = ChunkProver {
+            priv_id,
+            proving_key: chunk_pk,
+        };
+
+        c.bench_function(
+            &format!("Proving {} chunk(s) [cs={}]", num_chunks, chunk_size),
+            |b| {
+                b.iter(|| {
+                    chunk_prover
+                        .prove(&mut rng, &chunk)
+                        .expect("couldn't prove chunk");
+                })
+            },
+        );
+    }
+}
+
 /// This experiment constructs a blocklist and times how long it takes to prove IWF, the last
-/// chunk, and the aggregates thereof. The blocklist is split into head and tail. We call the tail
-/// the "buffer" and it consists of 14 chunks of size 16. We condition on head chunk size and
-/// existence of the tail. In the case that the tail exists, the "last chunk" proof is one of size
-/// 16. In the case that the tail does not exist, the "last chunk" proof is one of the head chunk
-/// size.
+/// chunk, and the aggregates thereof. It also computes verification throughput.
+///
+/// The blocklist is split into head and tail. We call the tail the "buffer" and it consists of 14
+/// chunks of size 16. For attestation, we condition on head chunk size and existence of the tail.
+/// In the case that the tail exists, the "last chunk" proof is one of size 16. In the case that
+/// the tail does not exist, the "last chunk" proof is one of the head chunk size.
+///
+/// For verification throughput, we measure the time it takes to do 256 buffered verifications in
+/// parallel, using the parameters set above.
 fn full_attestation(c: &mut Criterion) {
     let mut rng = test_rng();
 
@@ -147,10 +212,10 @@ fn full_attestation(c: &mut Criterion) {
             };
 
             for buffered in [true, false] {
-                let buf_str = if buffered { "Buffered" } else { "Unbuffered" };
+                let buf_str = if buffered { "buf" } else { "nobuf" };
                 c.bench_function(
                     &format!(
-                        "{} e2e with {} many {}-elem head chunks",
+                        "Proving 1 SB attestation [{},nc={},cs={}]",
                         buf_str, num_head_chunks, head_chunk_size
                     ),
                     |b| {
@@ -210,7 +275,7 @@ fn full_attestation(c: &mut Criterion) {
                 );
             }
 
-            // Just make sure that we're still producing valid proofs. Make a buffered one and verify it
+            // Now measure buffered verification throughput. Make the proof again
             let agg_head_chunk_proof = agg_head_chunk_prover
                 .prove(&mut rng, &mut head_chunk_proofs, &mut prepared_head_chunks)
                 .expect("couldn't prove HiCIAP over head chunks");
@@ -218,22 +283,33 @@ fn full_attestation(c: &mut Criterion) {
                 .prove(&mut rng, &mut tail_chunk_proofs, &mut prepared_tail_chunks)
                 .expect("couldn't prove HiCIAP over tail chunks");
 
-            let snarkblock_proof = SnarkblockProof::new(
+            let buffered_snarkblock_proof = SnarkblockProof::new(
                 &mut rng,
                 agg_iwf_proof,
                 vec![agg_head_chunk_proof, agg_tail_chunk_proof],
             );
 
-            assert!(snarkblock_verifier
-                .verify(
-                    vec![blocklist_head_com, blocklist_tail_com],
-                    &blocklist_elem,
-                    snarkblock_proof
-                )
-                .unwrap());
+            // Now verify
+            c.bench_function(
+                &format!(
+                    "Verifying 256 SB proofs [buf,nc={},cs={}]",
+                    num_head_chunks, head_chunk_size
+                ),
+                |b| {
+                    b.iter(|| {
+                        assert!(snarkblock_verifier
+                            .verify(
+                                vec![blocklist_head_com.clone(), blocklist_tail_com.clone()],
+                                &blocklist_elem,
+                                buffered_snarkblock_proof.clone(),
+                            )
+                            .unwrap());
+                    })
+                },
+            );
         }
     }
 }
 
-criterion_group!(benches, full_attestation);
+criterion_group!(benches, chunk_proof, full_attestation);
 criterion_main!(benches);
